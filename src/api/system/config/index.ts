e3361428aa049f77b4bdb3a2618ec0c82b5ccc05
microrain <Xtj@83668163>
2022-08-11 07:13:34 +0800

    修改参数配置api地址

--- src/api/system/config/index.ts
+++ src/api/system/config/index.ts
@@ -3,7 +3,7 @@ import request from '/@/utils/request';
 
 export function getConfigList(query:Object) {
     return request({
-        url: '/system/config/list',
+        url: '/common/config/list',
         method: 'get',
         params:query
     })
@@ -11,7 +11,7 @@ export function getConfigList(query:Object) {
 
 export function getConfig(id:number) {
     return request({
-        url: '/system/config/get',
+        url: '/common/config/get',
         method: 'get',
         params:{id}
     })
@@ -19,7 +19,7 @@ export function getConfig(id:number) {
 
 export function addConfig(data:any) {
     return request({
-        url: '/system/config/add',
+        url: '/common/config/add',
         method: 'post',
         data:data
     })
@@ -27,7 +27,7 @@ export function addConfig(data:any) {
 
 export function editConfig(data:any) {
     return request({
-        url: '/system/config/edit',
+        url: '/common/config/edit',
         method: 'put',
         data:data
     })
@@ -35,7 +35,7 @@ export function editConfig(data:any) {
 
 export function deleteConfig(ids:number[]) {
     return request({
-        url: '/system/config/delete',
+        url: '/common/config/delete',
         method: 'delete',
         data:{ids}
     })
