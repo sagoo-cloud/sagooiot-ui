98fec6e0f531f322f8a2931697a3150e9337c207
yukai <133373318@qq.com>
2022-08-29 11:46:19 +0800

    根据数据架构调整form表单

--- src/views/datahub/modeling/component/edit.vue
+++ src/views/datahub/modeling/component/edit.vue
@@ -1,78 +1,18 @@
 <template>
 	<div class="system-edit-dic-container">
-		<el-dialog :title="(ruleForm.sourceId !== 0 ? '修改' : '添加') + '数据源'" v-model="isShowDialog" width="769px">
+		<el-dialog :title="(ruleForm.sourceId !== 0 ? '修改' : '添加') + '自建表'" v-model="isShowDialog" width="769px">
 			<el-form :model="ruleForm" ref="formRef" :rules="rules" size="default" label-width="110px">
-				<el-form-item label="数据源标识" prop="key">
-					<el-input v-model="ruleForm.key" placeholder="请输入数据源名称" />
+				<el-form-item label="自建表标识" prop="key">
+					<el-input v-model="ruleForm.key" placeholder="请输入自建表名称" />
 				</el-form-item>
-				<el-form-item label="数据源名称" prop="name">
-					<el-input v-model="ruleForm.name" placeholder="请输入数据源名称" />
+				<el-form-item label="自建表名称" prop="name">
+					<el-input v-model="ruleForm.name" placeholder="请输入自建表名称" />
 				</el-form-item>
 
 				<el-form-item label="描述" prop="description">
 					<el-input v-model="ruleForm.description" type="textarea" placeholder="请输入内容"></el-input>
 				</el-form-item>
 
-				<el-form-item label="数据来源" prop="from">
-					<el-radio-group v-model="ruleForm.from">
-						<el-radio :label="1">api导入</el-radio>
-						<el-radio :label="2">数据库</el-radio>
-						<el-radio :label="3">文件</el-radio>
-					</el-radio-group>
-				</el-form-item>
-
-				<el-divider content-position="left">规则表达式</el-divider>
-
-				<div v-for="(item, index) in rule" :key="index">
-					<el-form-item label="表达式">
-						<el-input v-model="item.expression" placeholder="请输入规则表达式" />
-					</el-form-item>
-
-					<el-form-item label="参数" >
-						<el-input v-model="item.params.name" placeholder="请输入键值" class="w-35" />
-						<el-input v-model="item.params.value" placeholder="请输入值" class="w-35" />
-						<div class="conicon">
-							<el-icon @click="delRule(index)" v-if="index > 0"><Delete /></el-icon>
-						</div>
-					</el-form-item>
-				</div>
-				<div style="padding: 10px">
-					<el-button type="primary" class="addbutton" @click="addRule">增加</el-button>
-				</div>
-				<el-divider content-position="left">数据源配置</el-divider>
-
-				<el-form-item label="请求方法" >
-					<el-select v-model="config.method" placeholder="请选择请求方法">
-						<el-option v-for="item in methodData" :key="item.value" :label="item.label" :value="item.value" />
-					</el-select>
-				</el-form-item>
-
-				<el-form-item label="请求地址" >
-					<el-input v-model="config.url" placeholder="请输入请求地址" />
-				</el-form-item>
-
-				<el-form-item label="更新时间" >
-					<el-input v-model="config.interval" placeholder="请输入更新时间" class="w-35" />
-					<el-select v-model="config.intervalUnit" placeholder="请选择单位">
-						<el-option v-for="item in unitData" :key="item.value" :label="item.label" :value="item.value" />
-					</el-select>
-				</el-form-item>
-
-				<div class="box-content">
-					<el-divider content-position="left">请求参数</el-divider>
-					<div class="content-f" v-for="(item, index) in requestParams" :key="index">
-						<el-select v-model="item.type" placeholder="参数类型" style="width: 320px">
-							<el-option v-for="item in paramData" :key="item.value" :label="item.label" :value="item.value" />
-						</el-select>
-						<el-input v-model="item.name" placeholder="请输入参数标题" style="width: 320px" />
-						<el-input v-model="item.key" placeholder="请输入参数名" style="width: 320px" />
-						<el-input v-model="item.value" placeholder="请输入参数值" style="width: 320px" />
-						<div class="conicon">
-							<el-icon @click="delParams(index)" v-if="index > 0"><Delete /></el-icon>
-						</div>
-					</div>
-					<el-button type="primary" class="addbutton" @click="addParams">增加</el-button>
-				</div>
 			</el-form>
 			<template #footer>
 				<span class="dialog-footer">
@@ -91,12 +31,9 @@ import { ElMessage } from 'element-plus';
 import { Delete, Minus, Right } from '@element-plus/icons-vue';
 
 interface RuleFormState {
-	sourceId: number;
+	id: number;
 	name: string;
-	from: number;
 	key: string;
-	userName: string;
-	password: string;
 	description: string;
 	status: number;
 }
@@ -115,78 +52,7 @@ export default defineComponent({
 		const state = reactive<DicState>({
 			
 			isShowDialog: false,
-			config: {},
-			ruledata: [
-				{
-					expression: '',
-					params: {},
-				},
-			],
-			rule: [
-				{
-					expression: '',
-					params: {
-						name: '',
-						value: '',
-					},
-				},
-			],
-			requestParams: [
-				{
-					type: '',
-					key: '',
-					name: '',
-					value: '',
-				},
-			],
-			methodData: [
-				{
-					label: 'GET',
-					value: 'get',
-				},
-				{
-					label: 'POST',
-					value: 'post',
-				},
-				{
-					label: 'PUT',
-					value: 'put',
-				},
-			],
-
-			unitData: [
-				{
-					label: '秒',
-					value: 'second',
-				},
-				{
-					label: '分',
-					value: 'minute',
-				},
-				{
-					label: '时',
-					value: 'hour',
-				},
-				{
-					label: '天',
-					value: 'day',
-				},
-			],
-
-			paramData: [
-				{
-					lable: 'header',
-					value: 'header',
-				},
-				{
-					lable: 'body',
-					value: 'body',
-				},
-				{
-					lable: 'param',
-					value: 'param',
-				},
-			],
+			
 
 			ruleForm: {
 				sourceId: 0,
@@ -204,9 +70,9 @@ export default defineComponent({
 				description: '',
 			},
 			rules: {
-				key: [{ required: true, message: '数据源标识不能为空', trigger: 'blur' }],
-				name: [{ required: true, message: '数据源名称不能为空', trigger: 'blur' }],
-				from: [{ required: true, message: '数据源类型不能为空', trigger: 'blur' }],
+				key: [{ required: true, message: '自建表标识不能为空', trigger: 'blur' }],
+				name: [{ required: true, message: '自建表名称不能为空', trigger: 'blur' }],
+				from: [{ required: true, message: '自建表类型不能为空', trigger: 'blur' }],
 			
 			},
 		});
@@ -302,7 +168,7 @@ export default defineComponent({
 					if (state.ruleForm.sourceId !== 0) {
 						//修改
 						api.common.edit(state.ruleForm).then(() => {
-							ElMessage.success('数据源类型修改成功');
+							ElMessage.success('自建表类型修改成功');
 							closeDialog(); // 关闭弹窗
 							emit('typeList');
 						});
@@ -310,7 +176,7 @@ export default defineComponent({
 						//添加
 
 						api.common.add(state.ruleForm).then(() => {
-							ElMessage.success('数据源类型添加成功');
+							ElMessage.success('自建表类型添加成功');
 							closeDialog(); // 关闭弹窗
 							emit('typeList');
 						});
