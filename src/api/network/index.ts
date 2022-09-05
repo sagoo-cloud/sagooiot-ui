35366f0350c9b1d218c6dca24e7ffa241e2d1ecf
yanglzh <lgm122@qq.com>
2022-09-05 20:36:01 +0800

    去掉多余配置

--- src/api/network/index.ts
+++ src/api/network/index.ts
@@ -6,11 +6,11 @@
  * @FilePath: /sagoo-admin-ui/src/api/network/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { get, post, del, put } from '/@/utils/request_network';
+import { get, post, del } from '/@/utils/request';
 
 export default {
-  getList: (params?: object) => get('/tunnel/list', params),
-  deleteItem: (params?: object) => del('/tunnel/delete', params),
-  getDetail: (params?: object) => get('/tunnel/get', params),
-  changeTunneStatus: (params?: object) => post('/tunnel/status', params),
+  getList: (params?: object) => get('/network/tunnel/list', params),
+  deleteItem: (params?: object) => del('/network/tunnel/delete', params),
+  getDetail: (params?: object) => get('/network/tunnel/get', params),
+  changeTunneStatus: (params?: object) => post('/network/tunnel/status', params),
 }
