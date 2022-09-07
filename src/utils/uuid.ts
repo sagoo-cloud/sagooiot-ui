64d7acea9445716f7fb7fd44359a3d207fcc1cb5
yanglzh <lgm122@qq.com>
2022-09-07 22:31:20 +0800

    增加大屏设计页面，增加新增，查询，编辑跳转功能

---
+++ src/utils/uuid.ts
@@ -0,0 +1,8 @@
+
+/**
+ * * 生成一个不重复的ID
+ * @param { Number } randomLength
+ */
+export const getUUID = (randomLength = 10) => {
+  return Number(Math.random().toString().substr(2, randomLength) + Date.now()).toString(36)
+}
