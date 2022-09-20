f68462ad0a66a0d024f67b28e57ea9d6e292c3d6
yanglzh <lgm122@qq.com>
2022-09-20 22:47:56 +0800

    完善组态列表的查询，编辑，删除，新建功能

--- src/api/configuration/index.ts
+++ src/api/configuration/index.ts
@@ -2,7 +2,7 @@ import { get, del, post } from '/@/utils/request';
 
 export default {
   getFolder: () => get('/configure/folder/list'),
-  getList: (folderId: number) => get('/configure/diagram/getInfoByFolderId', { folderId }),
-  del: (ids: string[]) => del('/screen/project/delProjectById', { ids }),
+  getList: (data: any) => get('/configure/diagram/getList', data),
+  del: (id: number) => del('/configure/diagram/del', { id }),
   add: (data: any) => post('/screen/project/add', data),
 }
