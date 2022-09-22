0586accc7d332bd5d24a2b916aaa5ca8c0278726
yanglzh <lgm122@qq.com>
2022-09-22 22:04:08 +0800

    增加loading

--- src/views/system/config/index.vue
+++ src/views/system/config/index.vue
@@ -1,74 +1,89 @@
 <template>
-  <div class="system-dic-container">
-    <el-card shadow="hover">
-      <div class="system-user-search mb15">
-        <el-form :model="tableData.param" ref="queryRef" :inline="true" label-width="68px">
-          <el-form-item label="参数名称" prop="configName">
-            <el-input v-model="tableData.param.configName" placeholder="请输入参数名称" clearable size="default" @keyup.enter.native="dataList" />
-          </el-form-item>
-          <el-form-item label="参数键名" prop="configKey">
-            <el-input v-model="tableData.param.configKey" placeholder="请输入参数键名" clearable size="default" @keyup.enter.native="dataList" />
-          </el-form-item>
-          <el-form-item label="系统内置" prop="configType" style="width: 200px;">
-            <el-select v-model="tableData.param.configType" placeholder="系统内置" clearable size="default" style="width: 240px">
-              <el-option v-for="dict in sys_yes_no" :key="dict.value" :label="dict.label" :value="dict.value" />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="创建时间" prop="dateRange">
-            <el-date-picker v-model="tableData.param.dateRange" size="default" style="width: 240px" value-format="YYYY-MM-DD" type="daterange" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期"></el-date-picker>
-          </el-form-item>
-          <el-form-item>
-            <el-button size="default" type="primary" class="ml10" @click="dataList">
-              <el-icon>
-                <ele-Search />
-              </el-icon>
-              查询
-            </el-button>
-            <el-button size="default" @click="resetQuery(queryRef)">
-              <el-icon>
-                <ele-Refresh />
-              </el-icon>
-              重置
-            </el-button>
-            <el-button size="default" type="success" class="ml10" @click="onOpenAddDic">
-              <el-icon>
-                <ele-FolderAdd />
-              </el-icon>
-              新增参数
-            </el-button>
-            <el-button size="default" type="danger" class="ml10" @click="onRowDel(null)">
-              <el-icon>
-                <ele-Delete />
-              </el-icon>
-              删除参数
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-      <el-table :data="tableData.data" style="width: 100%" @selection-change="handleSelectionChange">
-        <el-table-column type="selection" width="55" align="center" />
-        <el-table-column label="ID" align="center" prop="configId" width="60" />
-        <el-table-column label="参数名称" prop="configName" :show-overflow-tooltip="true" />
-        <el-table-column label="参数键名" prop="configKey" :show-overflow-tooltip="true" />
-        <el-table-column label="参数键值" prop="configValue" />
-        <el-table-column label="备注" prop="remark" :show-overflow-tooltip="true" />
-        <!-- <el-table-column label="创建时间" prop="createdAt" width="180" align="center" /> -->
-        <el-table-column label="系统内置" align="center" prop="configType" width="100">
-          <template #default="{row}">
-            {{row.configType?'是':'否'}}
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" width="100" align="center" fixed="right">
-          <template #default="scope">
-            <el-button size="small" text type="warning" @click="onOpenEditDic(scope.row)">修改</el-button>
-            <el-button size="small" text type="danger" @click="onRowDel(scope.row)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <pagination v-show="tableData.total>0" :total="tableData.total" v-model:page="tableData.param.pageNum" v-model:limit="tableData.param.pageSize" @pagination="dataList" />
-    </el-card>
-    <EditConfig ref="editDicRef" @dataList="dataList" :sysYesNoOptions="sys_yes_no" />
-  </div>
+	<div class="system-dic-container">
+		<el-card shadow="hover">
+			<div class="system-user-search mb15">
+				<el-form :model="tableData.param" ref="queryRef" :inline="true" label-width="68px">
+					<el-form-item label="参数名称" prop="configName">
+						<el-input v-model="tableData.param.configName" placeholder="请输入参数名称" clearable size="default" @keyup.enter.native="dataList" />
+					</el-form-item>
+					<el-form-item label="参数键名" prop="configKey">
+						<el-input v-model="tableData.param.configKey" placeholder="请输入参数键名" clearable size="default" @keyup.enter.native="dataList" />
+					</el-form-item>
+					<el-form-item label="系统内置" prop="configType" style="width: 200px">
+						<el-select v-model="tableData.param.configType" placeholder="系统内置" clearable size="default" style="width: 240px">
+							<el-option v-for="dict in sys_yes_no" :key="dict.value" :label="dict.label" :value="dict.value" />
+						</el-select>
+					</el-form-item>
+					<el-form-item label="创建时间" prop="dateRange">
+						<el-date-picker
+							v-model="tableData.param.dateRange"
+							size="default"
+							style="width: 240px"
+							value-format="YYYY-MM-DD"
+							type="daterange"
+							range-separator="-"
+							start-placeholder="开始日期"
+							end-placeholder="结束日期"
+						></el-date-picker>
+					</el-form-item>
+					<el-form-item>
+						<el-button size="default" type="primary" class="ml10" @click="dataList">
+							<el-icon>
+								<ele-Search />
+							</el-icon>
+							查询
+						</el-button>
+						<el-button size="default" @click="resetQuery(queryRef)">
+							<el-icon>
+								<ele-Refresh />
+							</el-icon>
+							重置
+						</el-button>
+						<el-button size="default" type="success" class="ml10" @click="onOpenAddDic">
+							<el-icon>
+								<ele-FolderAdd />
+							</el-icon>
+							新增参数
+						</el-button>
+						<el-button size="default" type="danger" class="ml10" @click="onRowDel(null)">
+							<el-icon>
+								<ele-Delete />
+							</el-icon>
+							删除参数
+						</el-button>
+					</el-form-item>
+				</el-form>
+			</div>
+			<el-table :data="tableData.data" style="width: 100%" @selection-change="handleSelectionChange" v-loading="tableData.loading">
+				<el-table-column type="selection" width="55" align="center" />
+				<el-table-column label="ID" align="center" prop="configId" width="60" />
+				<el-table-column label="参数名称" prop="configName" :show-overflow-tooltip="true" />
+				<el-table-column label="参数键名" prop="configKey" :show-overflow-tooltip="true" />
+				<el-table-column label="参数键值" prop="configValue" />
+				<el-table-column label="备注" prop="remark" :show-overflow-tooltip="true" />
+				<!-- <el-table-column label="创建时间" prop="createdAt" width="180" align="center" /> -->
+				<el-table-column label="系统内置" align="center" prop="configType" width="100">
+					<template #default="{ row }">
+						{{ row.configType ? '是' : '否' }}
+					</template>
+				</el-table-column>
+				<el-table-column label="操作" width="100" align="center" fixed="right">
+					<template #default="scope">
+						<el-button size="small" text type="warning" @click="onOpenEditDic(scope.row)">修改</el-button>
+						<el-button size="small" text type="danger" @click="onRowDel(scope.row)">删除</el-button>
+					</template>
+				</el-table-column>
+			</el-table>
+			<pagination
+				v-show="tableData.total > 0"
+				:total="tableData.total"
+				v-model:page="tableData.param.pageNum"
+				v-model:limit="tableData.param.pageSize"
+				@pagination="dataList"
+			/>
+		</el-card>
+		<EditConfig ref="editDicRef" @dataList="dataList" :sysYesNoOptions="sys_yes_no" />
+	</div>
 </template>
 
 <script lang="ts">
@@ -134,10 +149,14 @@ export default defineComponent({
 			dataList();
 		};
 		const dataList = () => {
-			api.config.getList(state.tableData.param).then((res: any) => {
-				state.tableData.data = res.list;
-				state.tableData.total = res.total;
-			});
+			state.tableData.loading = true;
+			api.config
+				.getList(state.tableData.param)
+				.then((res: any) => {
+					state.tableData.data = res.list;
+					state.tableData.total = res.total;
+				})
+				.finally(() => (state.tableData.loading = false));
 		};
 		// 打开新增字典弹窗
 		const onOpenAddDic = () => {
@@ -188,10 +207,10 @@ export default defineComponent({
 		const handleSelectionChange = (selection: TableDataRow[]) => {
 			state.ids = selection.map((item) => item.configId);
 		};
-    // 参数系统内置字典翻译
-    const typeFormat=(row:TableDataRow) => {
-      return proxy.selectDictLabel(unref(sys_yes_no), row.configType);
-    };
+		// 参数系统内置字典翻译
+		const typeFormat = (row: TableDataRow) => {
+			return proxy.selectDictLabel(unref(sys_yes_no), row.configType);
+		};
 		return {
 			addDicRef,
 			editDicRef,
