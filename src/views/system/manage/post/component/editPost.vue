9552a00f51304213e84262bf59721a25acf2b80c
yanglzh <lgm122@qq.com>
2022-11-02 23:50:18 +0800

    修改文字

--- src/views/system/manage/post/component/editPost.vue
+++ src/views/system/manage/post/component/editPost.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="system-edit-post-container">
-    <el-dialog v-model="isShowDialog" :title="(!formData.postId?'添加':'修改')+'岗位'" width="769px">
-      <el-form ref="formRef" :model="formData" :rules="rules" size="default" label-width="90px">
-        <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
-          <el-form-item label="上级岗位">
-            <el-cascader :options="postData" :props="{ checkStrictly: true,emitPath: false, value: 'postId', label: 'postName' }" placeholder="请选择组织" clearable class="w100" v-model="formData.parentId">
-              <template #default="{ node, data }">
-                <span>{{ data.postName }}</span>
-                <span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
-              </template>
-            </el-cascader>
-          </el-form-item>
-        </el-col>
-        <el-form-item label="岗位名称" prop="postName">
-          <el-input v-model="formData.postName" placeholder="请输入岗位名称" />
-        </el-form-item>
-        <!-- <el-form-item label="岗位编码" prop="postCode">
+	<div class="system-edit-post-container">
+		<el-dialog v-model="isShowDialog" :title="(!formData.postId ? '添加' : '修改') + '岗位'" width="769px">
+			<el-form ref="formRef" :model="formData" :rules="rules" size="default" label-width="90px">
+				<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
+					<el-form-item label="上级岗位">
+						<el-cascader :options="postData" :props="{ checkStrictly: true, emitPath: false, value: 'postId', label: 'postName' }" placeholder="请选择岗位" clearable class="w100" v-model="formData.parentId">
+							<template #default="{ node, data }">
+								<span>{{ data.postName }}</span>
+								<span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
+							</template>
+						</el-cascader>
+					</el-form-item>
+				</el-col>
+				<el-form-item label="岗位名称" prop="postName">
+					<el-input v-model="formData.postName" placeholder="请输入岗位名称" />
+				</el-form-item>
+				<!-- <el-form-item label="岗位编码" prop="postCode">
           <el-input v-model="formData.postCode" placeholder="请输入编码名称" />
         </el-form-item> -->
-        <el-form-item label="岗位顺序" prop="postSort">
-          <el-input-number v-model="formData.postSort" controls-position="right" :min="0" />
-        </el-form-item>
-        <el-form-item label="岗位状态" prop="status">
-          <el-switch v-model="formData.status" :active-value="1" :inactive-value="0" inline-prompt active-text="启" inactive-text="禁"></el-switch>
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="formData.remark" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button @click="onCancel" size="default">取 消</el-button>
-          <el-button type="primary" @click="onSubmit" size="default" :loading="loading">{{formData.postId===0?'新 增':'修 改'}}</el-button>
-        </span>
-      </template>
-    </el-dialog>
-  </div>
+				<el-form-item label="岗位顺序" prop="postSort">
+					<el-input-number v-model="formData.postSort" controls-position="right" :min="0" />
+				</el-form-item>
+				<el-form-item label="岗位状态" prop="status">
+					<el-switch v-model="formData.status" :active-value="1" :inactive-value="0" inline-prompt active-text="启" inactive-text="禁"></el-switch>
+				</el-form-item>
+				<el-form-item label="备注" prop="remark">
+					<el-input v-model="formData.remark" type="textarea" placeholder="请输入内容" />
+				</el-form-item>
+			</el-form>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button @click="onCancel" size="default">取 消</el-button>
+					<el-button type="primary" @click="onSubmit" size="default" :loading="loading">{{ formData.postId === 0 ? '新 增' : '修 改' }}</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
 </template>
 
 <script lang="ts">
@@ -106,8 +106,8 @@ export default defineComponent({
 		// 打开弹窗
 		const openDialog = (row?: DialogRow) => {
 			resetForm();
-      api.post.getList({ status: 1 }).then((res: any) => {
-        // console.log(res)
+			api.post.getList({ status: 1 }).then((res: any) => {
+				// console.log(res)
 				state.postData = res || [];
 			});
 			if (row) {
@@ -190,6 +190,7 @@ export default defineComponent({
 	background: #fff none !important;
 	border-radius: 4px;
 }
+
 .system-edit-post-container {
 	.menu-data-tree {
 		border: var(--el-input-border, var(--el-border-base));
