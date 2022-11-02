4ec3b6e6700bb2affc322852b74c66ff8a63530c
yanglzh <lgm122@qq.com>
2022-11-02 23:41:28 +0800

    修复ws地址获取错误

--- src/utils/origin.ts
+++ src/utils/origin.ts
@@ -1,15 +1,16 @@
 export default function getOrigin(suffix: string = '', type: string = 'http') {
   if (type === 'ws') {
     if (import.meta.env.VITE_SERVER_PROTOCOL === 'https:') {
-      return `wss//${import.meta.env.VITE_SERVER_HOSTNAME}${suffix}`
+      return `wss://${import.meta.env.VITE_SERVER_HOSTNAME}${suffix}`
     } else if (!import.meta.env.VITE_SERVER_PROTOCOL) {
       if (window.location.protocol === 'https:') {
-        return `wss//${window.location.hostname}${suffix}`
+        return `wss://${window.location.hostname}${suffix}`
+      } else {
+        return `ws://${window.location.hostname}${suffix}`
       }
     }
-    return `ws//${import.meta.env.VITE_SERVER_HOSTNAME}${suffix}`
+    return `ws://${import.meta.env.VITE_SERVER_HOSTNAME}${suffix}`
   }
-
 
   if (import.meta.env.VITE_SERVER_HOSTNAME) {
     return `${import.meta.env.VITE_SERVER_PROTOCOL}//${import.meta.env.VITE_SERVER_HOSTNAME}${suffix}`
