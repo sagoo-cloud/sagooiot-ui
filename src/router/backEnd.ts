f249322ac97dcd6207bdf57c8753d8c003fa1c1d
yanglzh <lgm122@qq.com>
2022-12-15 00:08:12 +0800

    去掉多余代码，包括demo，dblint 和这部分的路由配置等

--- src/router/backEnd.ts
+++ src/router/backEnd.ts
@@ -2,7 +2,7 @@ import { store } from '/@/store/index.ts';
 import { Session } from '/@/utils/storage';
 import { NextLoading } from '/@/utils/loading';
 import { setAddRoute, setFilterMenuAndCacheTagsViewRoutes } from '/@/router/index';
-import { demoRoutes, dynamicRoutes } from '/@/router/route';
+import { dynamicRoutes } from '/@/router/route';
 import api from '/@/api/system';
 
 
@@ -43,7 +43,7 @@ export async function initBackEndControlRoutes() {
 	// 存储接口原始路由（未处理component），根据需求选择使用
 	store.dispatch('requestOldRoutes/setBackEndControlRoutes', JSON.parse(JSON.stringify(menuRoute)));
 	// 处理路由（component），替换 dynamicRoutes（/@/router/route）第一个顶级 children 的路由
-	dynamicRoutes[0].children?.push(...await backEndComponent(menuRoute), ...demoRoutes);
+	dynamicRoutes[0].children?.push(...await backEndComponent(menuRoute));
 	// 添加动态路由
 	await setAddRoute();
 	// 设置递归过滤有权限的路由到 vuex routesList 中（已处理成多级嵌套路由）及缓存多级嵌套数组处理后的一维数组
