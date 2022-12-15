9dd72eafffe7d1b20f918031d63bc6a97f33ca99
yanglzh <lgm122@qq.com>
2022-12-15 23:18:54 +0800

    修复主页和物联概览页面跳转页面不变的bug，因为部分代码有报错的情况，修复报错导致的代码阻塞

--- src/layout/main/defaults.vue
+++ src/layout/main/defaults.vue
@@ -33,7 +33,7 @@ export default defineComponent({
 		watch(
 			() => route.path,
 			() => {
-				proxy.$refs.layoutDefaultsScrollbarRef.wrap$.scrollTop = 0;
+				proxy.$refs.layoutDefaultsScrollbarRef.wrap$ && (proxy.$refs.layoutDefaultsScrollbarRef.wrap$.scrollTop = 0)
 			}
 		);
 		return {
