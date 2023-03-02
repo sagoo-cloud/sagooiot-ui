bc06611648f18175cfa5bd19f4272af2820db2e0
yanglzh <lgm122@qq.com>
2023-03-02 22:49:24 +0800

    能耗分析-绩效考核增加供热负荷、供电负荷

--- src/views/heating/energyAnalysis/loopEnergyCostPerformanceAppraisal/index.vue
+++ src/views/heating/energyAnalysis/loopEnergyCostPerformanceAppraisal/index.vue
@@ -77,6 +77,9 @@
 				<el-table-column :label="`电单耗(${unitMap['单日用电单耗']})`" prop="elctricConsumption" />
 				<el-table-column :label="`总耗水(${unitMap['日总水耗单位']})`" prop="flowLossTotal" />
 				<el-table-column :label="`水单耗(${unitMap['日失水单耗']})`" prop="flowLoss" />
+				<el-table-column :label="`供热负荷(${unitMap['供热负荷']})`" prop="unitConsumptionDemand" />
+				<el-table-column :label="`供电负荷(${unitMap['供电负荷']})`" min-width="100" prop="elctricConsumptionDemand" />
+				<!-- <el-table-column :label="`失水量(${unitMap['失水量']})`" prop="flowLoss" /> -->
 			</el-table>
 
 			<pagination
