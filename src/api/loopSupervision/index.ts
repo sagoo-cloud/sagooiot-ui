d57ff752a3e927535c0107effc8ea44170c8f28f
yanglzh <lgm122@qq.com>
2023-03-16 15:26:41 +0800

    修复文件下载

--- src/api/loopSupervision/index.ts
+++ src/api/loopSupervision/index.ts
@@ -1,9 +1,9 @@
-import { get, post, del, put } from '/@/utils/request';
+import { get, post, del, put, file } from '/@/utils/request';
 
 export default {
   getLoopHistoryData: (params: object) => get('/region/monitor/getLoopHistoryData', params), // 环路监管
   getLoopRegulation: (params: object) => get('/region/monitor/getLoopRegulation', params), // 环路监管
   getAllHeatStation: (params: object) => get('/region/monitor/getAllHeatStation', params), // 分布图
   getLoopRegulationDetail: (params: object) => get('/region/monitor/getLoopRegulationDetail', params), // 环路监管详情
-  loopReportExport: (params: object) => get('/region/monitor/loopReportExport', params), // 环路监管详情列表导出
+  loopReportExport: (params: object) => file('/region/monitor/loopReportExport', params), // 环路监管详情列表导出
 }
