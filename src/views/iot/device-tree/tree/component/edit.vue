a4ffc6a45da71ff991afbb00491e0b7ee316d79b
picasso <929144387@qq.com>
2023-06-08 22:23:34 +0800

    设备树调整

--- src/views/iot/device-tree/tree/component/edit.vue
+++ src/views/iot/device-tree/tree/component/edit.vue
@@ -4,6 +4,21 @@
 			<el-form ref="formRef" :model="ruleForm" :rules="rules" size="default" label-width="120px">
 				<el-row :gutter="35">
 					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+						<el-form-item label="设备树" prop="parentId">
+              <el-tree-select
+                v-model="ruleForm.parentId"
+                :data="treeData"
+                check-strictly
+                style="width: 100%;"
+                :props="{
+                  label: 'name'
+                }"
+                node-key="infoId"
+                :render-after-expand="true"
+              />
+						</el-form-item>
+					</el-col>
+					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
 						<el-form-item label="名称" prop="name">
 							<el-input v-model="ruleForm.name" placeholder="请输入名称" clearable></el-input>
 						</el-form-item>
@@ -34,24 +49,11 @@
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
-						<el-form-item label="开始日期" prop="startDate">
-							<el-date-picker
-								v-model="ruleForm.startDate"
-								type="date"
-								placeholder="选择开始日期"
-								:size="'default'"
-							/>
-						</el-form-item>
-					</el-col>
-					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
-						<el-form-item label="结束日期" prop="endDate">
-							<el-date-picker
-								v-model="ruleForm.endDate"
-								type="date"
-								placeholder="选择结束日期"
-								:size="'default'"
-							/>
-						</el-form-item>
+            <el-form-item label="类型" prop="template">
+              <el-select v-model="ruleForm.template" filterable clearable placeholder="请选择类型" style="width: 100%;">
+                <el-option v-for="dict in tree_types" :key="dict.value" :label="dict.label" :value="dict.value"> </el-option>
+              </el-select>
+            </el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
 						<el-form-item label="设备标识" prop="deviceKey">
@@ -59,8 +61,22 @@
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+            <el-form-item label="分类" prop="category">
+              <el-select v-model="ruleForm.category" filterable clearable placeholder="请选择分类" style="width: 100%;">
+                <el-option v-for="dict in tree_category" :key="dict.value" :label="dict.label" :value="dict.value"> </el-option>
+              </el-select>
+            </el-form-item>
+					</el-col>
+					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
 						<el-form-item label="设备所属区域" prop="area">
-							<el-input v-model="ruleForm.area" placeholder="请输入设备所属区域" clearable></el-input>
+
+							<el-cascader :options="orgData" :props="{ checkStrictly: true, emitPath: false, value: 'id', label: 'name' }" placeholder="请选择区域" clearable class="w100" v-model="ruleForm.area">
+								<template #default="{ node, data }">
+									<span>{{ data.name }}</span>
+									<span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
+								</template>
+							</el-cascader>
+							<!-- <el-input v-model="ruleForm.area" placeholder="请输入设备所属区域" clearable></el-input> -->
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
@@ -70,7 +86,7 @@
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
 						<el-form-item label="类型" prop="types">
-							<el-select v-model="ruleForm.types" filterable clearable placeholder="请选择类型" class="w-35">
+							<el-select v-model="ruleForm.types" filterable clearable placeholder="请选择类型" style="width: 100%">
 								<el-option v-for="dict in tree_types_2" :key="dict.value" :label="dict.label" :value="dict.value"> </el-option>
 							</el-select>
 							<!-- <el-input v-model="ruleForm.types" placeholder="请输入所属公司" clearable></el-input> -->
@@ -91,11 +107,13 @@
 <script lang="ts">
 import { reactive, toRefs, defineComponent, ref, unref, getCurrentInstance } from 'vue';
 import api from '/@/api/device';
+import apiSystem from '/@/api/system';
 import { phoneValidate } from '/@/utils/validator';
 import { ElMessage } from 'element-plus';
 
 interface RuleFormState {
 	id?: number;
+	parentId: string|number;
 	name: string;
 	address: string;
 	lat: string;
@@ -112,6 +130,7 @@ interface RuleFormState {
 }
 
 const baseForm: RuleFormState = {
+	parentId: '',
 	name: '',
 	address: '',
 	lat: '',
@@ -141,9 +160,13 @@ export default defineComponent({
 				name: [{ required: true, message: '名称不能为空', trigger: 'blur' }],
 				address: [{ required: true, message: '地址不能为空', trigger: 'blur' }],
 			},
+      orgData: [],
+      treeData: []
 		});
 
-    const { tree_types_2 } = proxy.useDict('tree_types_2');
+    // const { tree_types_2 } = proxy.useDict('tree_types_2');
+
+    const { tree_types, tree_category } = proxy.useDict('tree_types', 'tree_category');
 
 		// 打开弹窗
 		const openDialog = (type: string, row?: any) => {
@@ -154,6 +177,13 @@ export default defineComponent({
 				state.ruleForm.id = row.infoId
 				getDetail()
 			}
+
+			apiSystem.org.getList({ status: 1 }).then((res: any) => {
+				state.orgData = res || [];
+			});
+      api.tree.getList({}).then((res: any) => {
+        state.treeData = res.list;
+      })
 			state.isShowDialog = true;
 		};
 		// 关闭弹窗
@@ -205,7 +235,9 @@ export default defineComponent({
 			onCancel,
 			onSubmit,
 			formRef,
-			tree_types_2,
+			// tree_types_2,
+      tree_types,
+      tree_category,
 			...toRefs(state),
 		};
 	},
