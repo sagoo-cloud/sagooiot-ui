20bfc6a0bafe77e1884a7b6c4568fd458ae9a739
yanglzh <lgm122@qq.com>
2023-11-27 09:41:22 +0800

    fix: 增加解密操作

--- src/utils/rsa.ts
+++ src/utils/rsa.ts
@@ -3,7 +3,17 @@ import jsrsasign from 'jsrsasign'
 // 初始化RSA密钥对象
 const publicKey = jsrsasign.KEYUTIL.getKey(import.meta.env.VITE_PUBLIC_KEY);
 
+// 从PEM格式的私钥创建RSA密钥对象
+const rsaPrivateKey = new jsrsasign.RSAKey();
+rsaPrivateKey.readPKCS8PrvKeyHex(jsrsasign.KEYUTIL.getHexFromPEM(import.meta.env.VITE_PRIVATE_KEY));
+
+// 加密 
 export function encrypt(str: string) {
-  const encrypted = jsrsasign.KJUR.crypto.Cipher.encrypt(str, publicKey, 'RSAOAEP256');
-  return jsrsasign.hextob64(encrypted)
+  return jsrsasign.hextob64(publicKey.encryptOAEP(str, 'sha-256'))
+}
+
+// 解密
+export function decrypt(encryptedBase64: string) {
+  const encryptedHex = jsrsasign.b64tohex(encryptedBase64);
+  return jsrsasign.hextob64(rsaPrivateKey.decryptOAEP(encryptedHex, 'sha-256'))
 }
