ffa2b170e329ec135510f325e53422ea5bbe30d6
yanglzh <lgm122@qq.com>
2023-11-27 09:30:03 +0800

    fix: 去掉多余log

--- src/utils/rsa.ts
+++ src/utils/rsa.ts
@@ -1,15 +1,9 @@
 import jsrsasign from 'jsrsasign'
 
-const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
-
 // 初始化RSA密钥对象
-const publicKey = jsrsasign.KEYUTIL.getKey(PUBLIC_KEY);
-
-console.log(publicKey)
-
+const publicKey = jsrsasign.KEYUTIL.getKey(import.meta.env.VITE_PUBLIC_KEY);
 
 export function encrypt(str: string) {
   const encrypted = jsrsasign.KJUR.crypto.Cipher.encrypt(str, publicKey, 'RSAOAEP256');
-  const resStr = jsrsasign.hextob64(encrypted)
-  return resStr
+  return jsrsasign.hextob64(encrypted)
 }
