1e86971e82d9e405ac4298b0ecd8a209a55d4423
yanglzh <lgm122@qq.com>
2023-11-27 11:20:49 +0800

    fix: 去除解密报错的代码

--- src/utils/rsa.ts
+++ src/utils/rsa.ts
@@ -3,17 +3,7 @@ import jsrsasign from 'jsrsasign'
 // 初始化RSA密钥对象
 const publicKey = jsrsasign.KEYUTIL.getKey(import.meta.env.VITE_PUBLIC_KEY);
 
-// 从PEM格式的私钥创建RSA密钥对象
-const rsaPrivateKey = new jsrsasign.RSAKey();
-rsaPrivateKey.readPKCS8PrvKeyHex(jsrsasign.KEYUTIL.getHexFromPEM(import.meta.env.VITE_PRIVATE_KEY));
-
 // 加密 
 export function encrypt(str: string) {
   return jsrsasign.hextob64(publicKey.encryptOAEP(str, 'sha-256'))
-}
-
-// 解密
-export function decrypt(encryptedBase64: string) {
-  const encryptedHex = jsrsasign.b64tohex(encryptedBase64);
-  return jsrsasign.hextob64(rsaPrivateKey.decryptOAEP(encryptedHex, 'sha-256'))
 }
